12955d526fffa512796d3ef33865eb069634009c
seiroise <seiroise@gmail.com>
2017-06-16 14:11:08 +0900

    testの修正

--- Assets/Polying/01_Scenes/10_Test/Scripts/LaserSite2D.cs
+++ Assets/Polying/01_Scenes/10_Test/Scripts/LaserSite2D.cs
@@ -6,27 +6,27 @@ namespace Polying.Test {
 	/// <summary>
 	/// レーザーサイト
 	/// </summary>
-	[RequireComponent(typeof(LineRenderer))]
 	public class LaserSite2D : MonoBehaviour {
 
 		[SerializeField, Range(10f, 1000f)]
 		private float _distance = 100f;
+
+		[Header("Laser Effect")]
 		[SerializeField]
 		private SpriteRenderer _laserSprite;
+		[SerializeField, Range(0f, 10f)]
+		private float width = 1f;
+
+		[Header("Laser Hit Effect")]
 		[SerializeField]
 		private Transform _laserHitSprite;
 
-		private LineRenderer _line;
 		private Transform _trans;
 		private Transform _laserTrans;
 
 		private float _laserScale;
 
 		private void Awake() {
-			_line = GetComponent<LineRenderer>();
-			_line.SetVertexCount(2);
-			_line.SetPosition(0, new Vector3(0f, 0f, 0f));
-			_line.SetPosition(1, new Vector3(0f, 0f, 0f));
 			_trans = transform;
 			_laserTrans = _laserSprite.transform;
 			_laserScale = _laserSprite.sprite.pixelsPerUnit / _laserSprite.sprite.rect.width;
@@ -39,12 +39,9 @@ namespace Polying.Test {
 		private void UpdateRaser() {
 			RaycastHit2D hit;
 			hit = Physics2D.Raycast(_trans.position, _trans.right, _distance);
-			_line.SetPosition(0, _trans.position);
-			_line.SetPosition(1, hit.point);
-			Debug.Log(hit.distance);
 			if(_laserSprite) {
-				_laserTrans.localPosition = new Vector3(hit.distance, 0f, 0f);
-				_laserTrans.localScale = new Vector3(hit.distance * _laserScale * 2f, 1f, 1f);
+				_laserTrans.localPosition = new Vector3(hit.distance * 0.5f, 0f, 0f);
+				_laserTrans.localScale = new Vector3(hit.distance * _laserScale, width, 1f);
 			}
 			if(_laserHitSprite) {
 				_laserHitSprite.position = hit.point;
---
+++ Assets/Polying/01_Scenes/10_Test/Scripts/ShakeTransform.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Polying.Test {
+
+	/// <summary>
+	/// Transformを揺らす
+	/// </summary>
+	public class ShakeTransform : MonoBehaviour {
+
+		private Vector2 dir;
+
+		private void Update() {
+			
+		}
+
+		/// <summary>
+		/// 揺らす
+		/// </summary>
+		/// <param name="force"></param>
+		public void Shake(float force) {
+			dir.x = Random.Range(-1f, 1f);
+		}
+	}
+}
